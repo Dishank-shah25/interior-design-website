271d5a079f0b1ef0486a61b5399e584b86e79e33
Dishank-shah25 <dishankshah2000@gmail.com>
2024-06-11 16:12:35 +0530

    ALLOWED_HOSTS = ['yourdomain.com', 'localhost', '13.60.36.21']

--- myproject/settings.py
+++ myproject/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'newapp.apps.NewappConfig',
+    'newapp',
 ]
 
 MIDDLEWARE = [
@@ -126,3 +126,5 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+ALLOWED_HOSTS = ['yourdomain.com', 'localhost', '13.60.36.21']
